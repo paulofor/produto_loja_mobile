b46fb270da55fefbea144e45ef2b66b8fee0dfc6
Paulo Alexandre <paulofore@gmail.com>
2018-11-01 21:58:43 -0300

    quase executando

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -3,15 +3,48 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { rotasPrincipal } from './rotas-principal';
+import { RouterModule } from '@angular/router';
+import { PrincipalComponent } from './principal/principal.component';
+import { LoginComponent } from './login/login.component';
+import { MatDialogModule, MatInputModule, MatButtonModule, MatToolbarModule, MatFormFieldModule } from '@angular/material';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { SocketConnection } from './shared/sdk/sockets/socket.connections';
+import { SocketDriver } from './shared/sdk/sockets/socket.driver';
+import { SDKModels, LoopBackAuth, InternalStorage } from './shared/sdk';
+import { BrowserAnimationsModule, NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PrincipalComponent,
+    LoginComponent
   ],
+  
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatDialogModule,
+    BrowserAnimationsModule,
+    NoopAnimationsModule,
+    MatToolbarModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+    HttpClientModule ,
+    RouterModule.forRoot(rotasPrincipal)
+
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    HttpClient, 
+    SocketConnection  , 
+    SocketDriver,
+    SDKModels,
+    LoopBackAuth,
+    InternalStorage
+  ],
+  bootstrap: [AppComponent],
+  entryComponents: [LoginComponent]
 })
 export class AppModule { }
