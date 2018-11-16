243b3001b74956c688fbbc5a0cf4fd9f9d7fd50c
Paulo <paulofore@gmail.com>
2018-11-16 19:20:24 -0200

    tela web

---
+++ front/src/app/tela/pedido-tela/pedido-tela.componente.ts
@@ -0,0 +1,19 @@
+
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tela-pedido',
+  templateUrl: './tela-pedido.component.html',
+  styleUrls: ['./tela-pedido.component.scss']
+})
+export class PedidoTelaComponent implements OnInit {
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+
+
+}
