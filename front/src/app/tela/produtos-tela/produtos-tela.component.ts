82bfdf7194f7fbf98015d5707356ce6076a874c3
Paulo <paulofore@gmail.com>
2018-11-17 16:19:41 -0200

    spec tela componente

---
+++ front/src/app/tela/produtos-tela/produtos-tela.component.ts
@@ -0,0 +1,19 @@
+
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tela-produtos',
+  templateUrl: './tela-produtos.component.html',
+  styleUrls: ['./tela-produtos.component.scss']
+})
+export class ProdutosTelaComponent implements OnInit {
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+
+
+}
